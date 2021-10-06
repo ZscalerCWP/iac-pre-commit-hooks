c7851128d960873b3b05b527acf7bfbaa522d6cd
abhijeetsinghrawat <abhijeet.rawat@zscaler.com>
2021-10-06 08:45:30 +0530

    Naming changes

--- scripts/iac_pre_commit_hook.py
+++ scripts/iac_pre_commit_hook.py
@@ -1,5 +1,10 @@
+from typing import Optional
+from typing import Sequence
+from typing import Set
+
+
 def main(argv: Optional[Sequence[str]] = None) -> int:
-    print "Hello"
+    print("Hello")
 
 
 if __name__ == '__main__':
